ab7daf1844f886781f30a12c002b6294961484e8
Maxi Santucho <maximilianosantucho@outlook.es>
2024-12-20 13:47:58 -0300

    ejercicio1

--- Logica/src/main/java/com/logica/Main.java
+++ Logica/src/main/java/com/logica/Main.java
@@ -1,17 +1,43 @@
 package com.logica;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void crecienteDecreciente() {
+
+    }
+
+//    public static void mezclarArray() {
+//        int[] a = {1, 2, 3, 7, 8, 9, 13, 14, 15, 19, 20, 21};
+//        int[] b = {4, 5, 6, 10, 11, 12, 16, 17, 18, 22, 23, 24};
+//        int[] c = new int[24];
+//        int num = 0;
+//        int j = 0;
+//        for (int i = 0; i < c.length; i++, j++) {
+//            c[i] = a[j];
+//            i++;
+//            j++;
+//            c[i] = a[j];
+//            j++;
+//            i++;
+//            c[i] = a[j];
+//            j -= 2;
+//            i++;
+//            c[i] = b[j];
+//            i++;
+//            j++;
+//            c[i] = b[j];
+//            j++;
+//            i++;
+//            c[i] = b[j];
+//        }
+//        for (int i = 0; i < c.length; i++) {
+//            System.out.print(c[i] + "; ");
+//        }
+//    }
+
+    public static void main(String[] args) {
+
+//        mezclarArray();
+
     }
 }
