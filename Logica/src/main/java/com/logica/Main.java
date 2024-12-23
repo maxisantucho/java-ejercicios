7af15731463690764949022890d315aed4c13a4b
Maxi Santucho <maximilianosantucho@outlook.es>
2024-12-23 11:03:39 -0300

    ejercicio12

--- Logica/src/main/java/com/logica/Main.java
+++ Logica/src/main/java/com/logica/Main.java
@@ -6,6 +6,47 @@ public class Main {
 
 //    static Scanner sc = new Scanner(System.in);
 
+//    public static void rellenarDiagonal() {
+//        int[][] matriz = new int[7][7];
+//        for (int i = 0; i < matriz.length; i++) {
+//            matriz[i][i] = 1;
+//        }
+//        for (int i = 0; i < matriz.length; i++) {
+//            for (int j = 0; j < matriz[i].length; j++) {
+//                System.out.print(matriz[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//    public static void matrizTraspuesta() {
+//        int[][] matriz = {{1, 2, 3},
+//                     {4, 5, 6},
+//                     {7, 8, 9}};
+//        System.out.println("Matriz original:");
+//        for (int i = 0; i < matriz.length; i++) {
+//            for (int j = 0; j < matriz[i].length; j++) {
+//                System.out.print(matriz[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//        int aux;
+//        for (int i = 0; i < matriz.length; i++) {
+//            for (int j = 0; j < i; j++) {
+//                aux = matriz[i][j];
+//                matriz[i][j] = matriz[j][i];
+//                matriz[j][i] = aux;
+//            }
+//        }
+//        System.out.println("Matriz traspuesta:");
+//        for (int i = 0; i < matriz.length; i++) {
+//            for (int j = 0; j < matriz[i].length; j++) {
+//                System.out.print(matriz[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+
 //    public static void sumarMatrices() {
 //        int[][] a = {{1, 3, 2},
 //                     {4, 3, 3},
@@ -341,6 +382,10 @@ public class Main {
 //    }
 
     public static void main(String[] args) {
+
+//        rellenarDiagonal();
+
+//        matrizTraspuesta();
 
 //        sumarMatrices();
 
