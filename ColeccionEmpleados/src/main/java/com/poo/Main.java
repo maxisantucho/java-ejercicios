f01ac73cc8072c54081eb7299a3adb751758b8bc
Maxi Santucho <maximilianosantucho@outlook.es>
2025-01-09 09:45:22 -0300

    fin.

--- ColeccionEmpleados/src/main/java/com/poo/Main.java
+++ ColeccionEmpleados/src/main/java/com/poo/Main.java
@@ -6,39 +6,40 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Persona pepe = new Persona("Pepe", "Perez");
-        Deportista mariano = new Deportista("Mariano", "Martinez", 100);
-        Deportista juan = new Deportista("Juan", "Juarez", 70);
-        Futbolista ana = new Futbolista("Ana", "Lopez", 80, 10);
-        Futbolista messi = new Futbolista("Andres", "Poccetinno", 100, 100);
-
-        ArrayList<Persona> personas = new ArrayList<>();
-        personas.add(pepe);
-        personas.add(mariano);
-        personas.add(juan);
-        personas.add(ana);
-        personas.add(messi);
-
-        ArrayList<Deportista> deportistas = new ArrayList<>();
-        deportistas.add(mariano);
-        deportistas.add(juan);
-
-        ArrayList<Futbolista> futbolistas = new ArrayList<>();
-        futbolistas.add(ana);
-        futbolistas.add(messi);
-
-        UtilListas.mostrarListaDePersonas(personas);
-        System.out.println();
-        UtilListas.mostrarListaDePersonas(deportistas);
-        System.out.println();
-        UtilListas.mostrarListaDePersonas(futbolistas);
-        System.out.println();
-
-        UtilListas.agregarDeportistaALaLista(deportistas, "Dibu", "Martineh", 16);
-        UtilListas.mostrarListaDePersonas(deportistas);
-        System.out.println();
-        UtilListas.agregarDeportistaALaLista(personas, "Alejandro", "Garnacho", 40);
-        UtilListas.mostrarListaDePersonas(personas);
+//        Persona pepe = new Persona("Pepe", "Perez");
+//        Deportista mariano = new Deportista("Mariano", "Martinez", 100);
+//        Deportista juan = new Deportista("Juan", "Juarez", 70);
+//        Futbolista ana = new Futbolista("Ana", "Lopez", 80, 10);
+//        Futbolista messi = new Futbolista("Andres", "Poccetinno", 100, 100);
+//
+//        ArrayList<Persona> personas = new ArrayList<>();
+//        personas.add(pepe);
+//        personas.add(mariano);
+//        personas.add(juan);
+//        personas.add(ana);
+//        personas.add(messi);
+//
+//
+//        ArrayList<Deportista> deportistas = new ArrayList<>();
+//        deportistas.add(mariano);
+//        deportistas.add(juan);
+//
+//        ArrayList<Futbolista> futbolistas = new ArrayList<>();
+//        futbolistas.add(ana);
+//        futbolistas.add(messi);
+//
+//        UtilListas.mostrarListaDePersonas(personas);
+//        System.out.println();
+//        UtilListas.mostrarListaDePersonas(deportistas);
+//        System.out.println();
+//        UtilListas.mostrarListaDePersonas(futbolistas);
+//        System.out.println();
+//
+//        UtilListas.agregarDeportistaALaLista(deportistas, "Dibu", "Martineh", 16);
+//        UtilListas.mostrarListaDePersonas(deportistas);
+//        System.out.println();
+//        UtilListas.agregarDeportistaALaLista(personas, "Alejandro", "Garnacho", 40);
+//        UtilListas.mostrarListaDePersonas(personas);
 
 
 //        ArrayList<Persona> personas = new ArrayList<>();
