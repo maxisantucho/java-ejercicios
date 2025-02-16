68be2419223870b43c2a7512ffb841dcb639bd48
Maxi Santucho <maximilianosantucho@outlook.es>
2025-02-16 15:07:01 -0300

    todo redi

--- Practica/src/main/java/clases/Main.java
+++ Practica/src/main/java/clases/Main.java
@@ -3,88 +3,9 @@ package clases;
 
 public class Main {
 
-    /*
-    * GIT
-    * start a working area (see also: git help tutorial)
-    * clone     Clone a repository into a new directory
-    * init      Create an empty Git repository or reinitialize an existing one
-    *
-    * work on the current change (see also: git help everyday)
-    * add       Add file contents to the index
-    * mv        Move or rename a file, a directory, or a symlink
-    * restore   Restore working tree files
-    * rm        Remove files from the working tree and from the index
-    *
-    * examine the history and state (see also: git help revisions)
-    * bisect    Use binary search to find the commit that introduced a bug
-    * diff      Show changes between commits, commit and working tree, etc
-    * grep      Print lines matching a pattern
-    * log       Show commit logs
-    * show      Show various types of objects
-    * status    Show the working tree status
-    *
-    * grow, mark and tweak your common history
-    * branch    List, create, or delete branches
-    * commit    Record changes to the repository
-    * merge     Join two or more development histories together
-    v rebase    Reapply commits on top of another base tip
-    * reset     Reset current HEAD to the specified state
-    * switch    Switch branches
-    * tag       Create, list, delete or verify a tag object signed with GPG
-    *
-    * collaborate (see also: git help workflows)
-    * fetch     Download objects and refs from another repository
-    * pull      Fetch from and integrate with another repository or a local branch
-    * push      Update remote refs along with associated objects
-    *
-    * 'git help -a' and 'git help -g' list available subcommands and some
-    * concept guides. See 'git help <command>' or 'git help <concept>'
-    * to read about a specific subcommand or concept.
-    * See 'git help git' for an overview of the system.
-    */
-
     public static void main(String[] args) {
 
-        // git
-        // ls
-        // cd
-        // cd ..
-        // pwd
-        // touch crea archivo
-        // echo "Crea archivo con texto" > nombre_archivo
-        // mkdir crea directorio
-        // git status
-        // git add
-        // git rm --cached nombredelarchivo
-        // git commit
-        // git log
-        // git checkout
-        // git ignore ???
-        // git diff
-        // git reset
-        // git reflog
-        // git tag no hace falta recordar los ID commit
-        // HEAD
-        // git branch
-        // git switch
-        // git merch ???
-        // alias (cuando quiero guardar un alias lo hago sin el git)
-        //git config --global alias.tree "log --oneline"
-        // git stash
-        // git remote
-        // git fetch
-        // git pull
-        // git clone
-        // git push
 
-//        maniana me cree un repo y le agrege 6 7 comit
-//        y practicar y probar comandos para entender
-//        como funciona internamente
-//        agregando archivos a stage, sacandolos
-//        asi mejor que rompa todo en un repo local
-//        que el dia de manana uno que este trabajand
-//        con mas personas jja
-//        que tambien te digo, voy a aprender un poco de bash
 
     }
 
